d53a43893c583fe3d29ef6e80b6cd4c795374d88
Ishpreet Kaur <ishpreetgulati10@gmail.com>
2020-08-30 16:09:54 +1200

    sum tree

--- day35.py
+++ day35.py
@@ -1,23 +1,26 @@
 """
-def dfs_path(r,sum,sum_val,input_matrix, count):
-    if sum == sum_val:
-      count[0] += 1
-    if 2*r + 1 < len(input_matrix) and input_matrix[2*r +1] != None:
-      dfs_path(2*r + 1, sum+ input_matrix[2*r +1],sum_val,input_matrix, count)
-    if 2*r + 2 < len(input_matrix) and input_matrix[2*r +2] != None:
-      dfs_path(2*r + 2,  sum+ input_matrix[2*r +2],sum_val,input_matrix, count)
-    return count[0]
+You are given a binary tree in which each node contains an integer value.Find the number of paths that sum to a given value.The path does not need to start or end at the root or a leaf, but it must go downwards (traveling only from parent nodes to child nodes).
+The tree has no more than 1,000 nodes and the values are in the range -1,000,000 to 1,000,000.
+Example:
+root = [10,5,-3,3,2,None,11,3,-2,None,1], sum = 8
 
-def sum_tree(input_matrix,sum_val):
-  count = [0]
-  for i in range(0, len(input_matrix)):
-    count1 = dfs_path(i,input_matrix[i],sum_val,input_matrix,count)
- 
-  return count1
-if __name__ == "__main__":
-  input_matrix = [10,5,-3,3,2,None,11,3,-2,None,1]
-  sum_val = 8
-  print(sum_tree(input_matrix,sum_val))
+      10
+     /  \
+    5   -3
+   / \    \
+  3   2   11
+ / \   \
+3  -2   1
+
+Return 3. The paths that sum to 8 are:
+
+1.  5 -> 3
+2.  5 -> 2 -> 1
+3. -3 -> 11
+
+
+
+
 
 """
 
